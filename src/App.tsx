bdede080fab4592324659542124665a31f69253e
Sumanbitw <007sumanghosh@gmail.com>
2021-05-23 22:36:15 +0530

    removed unecessary codes

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import './App.css';
 import Home from './components/Home/Home';
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Navbar from './components/Navbar/Navbar';
+import QuizTypes from './components/Quiz/QuizTypes';
+import QuizQuestion from './components/Quiz/QuizQuestion';
+import Score from './components/Quiz/Score';
 
 function App() {
   return (
   <div className="App">
-    <BrowserRouter>
     <Navbar/>
+    <BrowserRouter>
+    <div style={{marginTop:"5rem", zIndex:700}}>
     <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/quiz" element={<QuizTypes/>}/>
+    <Route path="/quiz/:id" element={<QuizQuestion/>}/>
+    <Route path="/score" element={<Score/>}/>
     </Routes>
+    </div>
     </BrowserRouter>
     </div>
   );
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "./home.css"
 import { Button, Typography } from '@material-ui/core'
 import '@fontsource/roboto'
-import { UseQuiz } from '../../context/quixContext';
-import { quizData } from "../../data/quiz"
-import { makeStyles, Theme } from "@material-ui/core/styles";
-import Icon from '@material-ui/core/Icon';
+// import { UseQuiz } from '../../context/quixContext';
+// import { quizOne } from "../../data/quiz1"
+import { makeStyles } from "@material-ui/core/styles";
+// import Icon from '@material-ui/core/Icon';
 import IconButton from "@material-ui/core/IconButton"
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward"
+import { Link } from "react-router-dom"
 
 const userStyles = makeStyles({
         headers : {
@@ -25,51 +26,8 @@ const userStyles = makeStyles({
 })
 function Home() {
     const classes = userStyles()
-  
-    // const [ currentQuestion, setCurrentQuestion ] = useState<number>(0)
-    // const [ score, setScore ] = useState<number>(0)
-    // const [ clicked, setClicked ] = useState<boolean>(false)
-    // const value = UseQuiz()
-    // function handleClick(answer: any) {
-    //     if(answer.isRight === true){
-    //         console.log("correct")
-    //         setScore(prevScore => prevScore + 1)
-    //     } else {
-    //         console.log("incorrect")
-    //         setScore(prevScore => prevScore - 1)
-    //     }
-    //     setClicked(true)
-    // }
 
-    // function handleNextQuestion(){
-    //     setClicked(false)
-    //     if(currentQuestion < quizData.questions.length - 1)
-    // {
-    //     setCurrentQuestion(prevQuestion => prevQuestion + 1)
-    // }
-    // }
     return (
-        // <div className="home">
-        //     <div className="home__container">
-        //     <div>
-            /* <Typography variant='h3' color="textSecondary">Sports Quiz 🤯</Typography>
-            <p>Status : {value?.status}</p>
-            <p>Score : {score}</p>
-            <div className="home__wrapper">
-                <h4>{quizData.quizType}</h4>
-               <div className="home__question">Q. {quizData.questions[currentQuestion].question} ?</div>
-               </div>
-               <div className="home__answers">{quizData.questions[currentQuestion].answers.map(answer => (
-                   <li className="answer__list">
-                       <button style={{ backgroundColor : clicked && answer.isRight ? "green" : "white" }} className="button" onClick={() => handleClick(answer)} disabled={clicked}>{answer.answer}</button>
-                   </li>
-               ))}
-               </div>
-               <button className="btn__next" onClick={handleNextQuestion} disabled={!clicked}>Next</button>
-            </div>
-            </div>
-        </div> */
-
         <div className="home">
             <Typography 
                 variant='h3' 
@@ -85,6 +43,7 @@ function Home() {
             </Typography> is a platform where you can test your knowledge in cricket. 
             </Typography>
 
+            <Link to="/quiz">
             <Button
                 type='submit'
                 variant='outlined'
@@ -93,6 +52,7 @@ function Home() {
             >
                 Explore 
             </Button>
+            </Link>
         </div>
     )
 }
